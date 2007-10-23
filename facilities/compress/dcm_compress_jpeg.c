6f975f50e21d6d240d678a2aa6c02d8eb0c342bd
Jason Summers <jsummers@mieweb.com>
2007-10-23 16:11:12 +0000

    Added strlcat function; I'm not sure how this used to work'

--- facilities/compress/dcm_compress_jpeg.c
+++ facilities/compress/dcm_compress_jpeg.c
@@ -96,6 +96,33 @@ struct my_destmgr_struct {
 int seg_header[] = {0xe000fffe, 0x00000000, 0xe000fffe, 0x00000000};
 int seg_footer[] = {0xe0ddfffe, 0x00000000};
 
+static size_t strlcat(char *dst, const char *src, size_t siz)
+{
+	register char *d = dst;
+	register const char *s = src;
+	register size_t n = siz;
+	size_t dlen;
+
+	/* Find the end of dst and adjust bytes left but don't go past end */
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = siz - dlen;
+
+	if (n == 0)
+		return(dlen + strlen(s));
+	while (*s != '\0') {
+		if (n != 1) {
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
+	*d = '\0';
+
+	return(dlen + (s - src));	/* count does not include NUL */
+}
+
 static void
 init_destination (j_compress_ptr cinfo)
 {
